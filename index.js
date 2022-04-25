438f4a13a31ee50201e8434cde1bd6a551693bd1
sbspanjers <sb.spanjers@gmail.com>
2022-04-25 15:19:19 +0200

    added get delete and get by id

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 const bodyParser = require('body-parser');
+const req = require('express/lib/request');
 
 app.use(bodyParser.json());
 
@@ -82,6 +83,8 @@ app.post('/api/user', (req, res) => {
       phoneNumber: user.phoneNumber,
       password: user.password,
     }
+    userDatabase.push(user);
+    console.log(userDatabase);
     res.status(200).json({
       status: 200,
       result: user,
@@ -94,8 +97,75 @@ app.post('/api/user', (req, res) => {
   }
 });
 
-app.delete('/api/user/delete:userId', (req, res) => { 
-  
+app.get('/api/user', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    result: userDatabase,
+  });
+});
+
+app.get('/api/user:userId', (req, res) => {
+  res.status(400).json({
+    status: 404,
+    result: 'Not implemented yet',
+  });
+});
+
+app.get('/api/user:userId', (req, res) => {
+  const userId = req.params.userId;
+  let user = userDatabase.filter((item) => item.id == userId);
+
+  if (user.length > 0) {
+    console.log(user);
+    res.status(200).json({
+      status: 200,
+      result: user,
+    });
+  } else {
+    res.status(404).json({
+      status: 404,
+      result: `Meal with ID ${userId} not found`,
+    });
+  }
+});
+
+app.put('/api/user:userId', (req, res) => { 
+  let user = req.body;
+  user = {
+    id: userId,
+    firstname: user.firstname,
+    lastname: user.lastname,
+    street: user.street,
+    city: user.city,
+    emailAdress: user.emailAdress,
+    phoneNumber: user.phoneNumber,
+    password: user.password,
+  }
+  userDatabase.put(user);
+  console.log(userDatabase);
+  res.status(200).json({
+    status: 200,
+    result: user,
+  });
+});
+
+app.delete('/api/user:userId', (req, res) => {
+  const userId = req.params.id;
+  let userIndex = userDatabase.findIndex((obj => obj.id == userId));
+
+  if (userIndex > -1) {
+    userDatabase.splice(userIndex, 1);
+
+    res.status(200).json({
+      status: 200,
+      result: 'User has been deleted',
+    });
+  } else {
+    res.status(400).json({
+      status: 405,
+      result: 'User has not been deleted',
+    });
+  }
 });
 
 app.all('*', (req, res) => {
