2833e62db29aa96b68d61d2e959593d20170356b
sbspanjers <sb.spanjers@gmail.com>
2022-05-22 23:01:16 +0200

    .

--- src/controllers/meal.controller.js
+++ src/controllers/meal.controller.js
@@ -53,13 +53,21 @@ let controller = {
     dbconnection.query(
       `INSERT INTO meal (name, description, price, maxAmountOfParticipants,isActive, isVega, isVegan, isToTakeHome, imageUrl, cookId, allergenes, dateTime) VALUES ('${meal.name}', '${meal.description}', ${meal.price}, ${meal.maxPersons},${meal.isActive}, ${meal.isVega}, ${meal.isVegan}, ${meal.isToTakeHome}, '${meal.imageUrl}', ${payload.userId}, '${meal.allergenes}' , '${meal.dateTime}')`,
       (err, results, fields) => {
-        const { affectedRows } = results;
-        if (affectedRows > 0) {
-          error = {
-            status: 201,
-            message: "Meal added",
-            result: { id: results.insertId, ...meal },
-          };
+        if (results != null) {
+          const { affectedRows } = results;
+          if (affectedRows > 0) {
+            error = {
+              status: 201,
+              message: "Meal added",
+              result: { id: results.insertId, ...meal },
+            };
+          } else {
+            error = {
+              status: 404,
+              message: "Meal not added",
+            };
+          }
+          next(error);
         } else {
           error = {
             status: 404,
