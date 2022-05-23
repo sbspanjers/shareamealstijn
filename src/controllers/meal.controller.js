83ac949eb358f21bd077d12bdee55df34c8cecec
sbspanjers <sb.spanjers@gmail.com>
2022-05-23 14:36:32 +0200

    meal

--- src/controllers/meal.controller.js
+++ src/controllers/meal.controller.js
@@ -51,7 +51,7 @@ let controller = {
     const payload = jwt.decode(token);
 
     dbconnection.query(
-      `INSERT INTO meal (name, description, price, maxAmountOfParticipants,isActive, isVega, isVegan, isToTakeHome, imageUrl, cookId, allergenes) VALUES ('${meal.name}', '${meal.description}', ${meal.price}, ${meal.maxPersons},${meal.isActive}, ${meal.isVega}, ${meal.isVegan}, ${meal.isToTakeHome}, '${meal.imageUrl}', ${payload.userId}, '${meal.allergenes}')`,
+      `INSERT INTO meal (name, description, price, maxAmountOfParticipants, isActive, isVega, isVegan, isToTakeHome, imageUrl, cookId, allergenes) VALUES ('${meal.name}', '${meal.description}', ${meal.price}, ${meal.maxPersons}, ${meal.isActive}, ${meal.isVega}, ${meal.isVegan}, ${meal.isToTakeHome}, '${meal.imageUrl}', ${payload.userId}, '${meal.allergenes}')`,
       (err, results, fields) => {
         if (results != null) {
           const { affectedRows } = results;
@@ -72,6 +72,7 @@ let controller = {
           error = {
             status: 404,
             message: "Meal not added",
+            err: err,
           };
         }
         next(error);
